cec23fc7fd44c71be00ae6f1bc4990f76e339b85
vai <ncysatnaf@gmail.com>
2016-05-18 10:02:29 +0800

    use babel

---
+++ app.js
@@ -0,0 +1,19 @@
+import koa from 'koa'
+import router from 'koa-router'
+
+export const app = new koa()
+let koarouter = router()
+ 
+
+koarouter.get('/', async function(ctx, next){
+  ctx.body = '<h1>Hello! This is my home page!</h1>'
+})
+koarouter.get('/about', async function(ctx, next){
+  ctx.body = '<h1>My name is Vai!</h1>'
+})
+
+app.use(koarouter.routes()).use(koarouter.allowedMethods())
+
+
+app.listen(8008)
+console.log('koa listening on port 8008')
--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-import koa from 'koa'
-import router from 'koa-router'
+'use strict'
 
-const app = new koa()
-let koarouter = router()
- 
-
-koarouter.get('/', async function(ctx, next){
-  ctx.body = '<h1>Hello! This is my home page!</h1>'
-})
-koarouter.get('/about', async function(ctx, next){
-  ctx.body = '<h1>My name is Vai!</h1>'
-})
-
-app.use(koarouter.routes()).use(koarouter.allowedMethods())
-
-
-app.listen(8008)
-console.log('koa listening on port 8008')
+require('babel-polyfill')
+require('babel-core/register')
+require('./app')
